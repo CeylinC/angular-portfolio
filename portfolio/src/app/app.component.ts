8476a85c86acc8e6b8b349258118d57e3186098e
Ceylin <caltepeceylin@gmail.com>
2025-05-31 18:09:31 +0300

    feat: add hero image

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from './components/button/button.component';
+import { HeroImageComponent } from "./components/hero-image/hero-image.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonComponent],
+  imports: [RouterOutlet, ButtonComponent, HeroImageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
