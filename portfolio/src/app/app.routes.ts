9f4f79c396cb4e9fe5d4a67be090f0e67bb0b5b0
Ceylin <caltepeceylin@gmail.com>
2025-06-01 15:51:23 +0300

    feat: add lazy loading and routes

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layouts/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
+      },
+      {
+        path: 'about',
+        loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent),
+      },
+      {
+        path: 'projects',
+        loadComponent: () => import('./pages/projects/projects.component').then(m => m.ProjectsComponent),
+      },
+      { path: '', redirectTo: '', pathMatch: 'full' },
+    ]
+  },
+];
