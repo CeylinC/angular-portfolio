5b751cd4cf9b57ef2d922802c13618a08495546c
Ceylin <caltepeceylin@gmail.com>
2025-06-01 20:18:35 +0300

    feat: add navbar

--- portfolio/src/app/layouts/layout/layout.component.ts
+++ portfolio/src/app/layouts/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "../../components/footer/footer.component";
+import { NavbarComponent } from "../../components/navbar/navbar.component";
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
